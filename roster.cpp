be29b576c61b2eee2e33288f552a5cbf8b664de7
Cody Buller <codybuller325@gmail.com>
2024-09-04 21:51:03 -0600

    Updated 9-4

--- roster.cpp
+++ roster.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "roster.h"
 
 using namespace std;
 
-Array classRosterAray[7,5];
+void Roster::add(string studentID, string firstName, string lastName, string emailAddress, int age, int daysInCourse1, int daysInCourse2, int daysInCourse3, DegreeProgram degreeProgram) {
+    for (int i = 0; i < 5; i++) {
+        if (classRosterArray[i] != nullptr) {
+            classRosterArray[i] = new Student();
+        }
+    }
+}
+
+void Roster::remove(string studentID) {
+    if (classrosterArray[i])
+}
+
---
+++ roster.h
@@ -0,0 +1,35 @@
+#include "student.h"
+#include "degree.h"
+
+using namespace std;
+
+#ifndef ROSTER_H
+#define ROSTER_H
+
+class Roster {
+    public:
+
+    Roster();
+    void printAll();
+    void printAverageDaysInCourse(string studentID);
+    void printInvalidEmails();
+    void remove(string studentID);
+    void printByDegreeProgram(DegreeProgram degreeProgram);
+
+    void add(
+        string studentID, 
+        string firstName, 
+        string lastName, 
+        string emailAddress, 
+        int age, 
+        int daysInCourse1, 
+        int daysInCourse2, 
+        int daysInCourse3, 
+        DegreeProgram degreeProgram
+        );
+
+    private:
+    Student* classRosterArray[7,5];
+};
+
+#endif
